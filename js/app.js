15be270b639bec3b244d0886407a5dd2f632ec95
Tina Johnson <tinajohnson.1234@gmail.com>
2016-05-03 12:16:48 +0530

    Add focus to root on loading and minor cleanup

--- js/app.js
+++ js/app.js
@@ -1,54 +1,5 @@
 var app = angular.module( 'hierarchyTree', [] );
 
-app.controller("mainController", function($http, $scope) {
-    $scope.familyTree = null;
-    $scope.sons = [];
-    $scope.daughters = [];
-    $scope.husbands = [];
-    $scope.wives = [];
-    $http.get( 'data/familyTree.json')
-        .success( function( data ) {
-            $scope.familyTree = data;
-
-            //assuming that root has an id:1 always
-            for (var i=0; i<$scope.familyTree.length; i++) {
-                var obj = $scope.familyTree[i];
-                if( obj.relationWith == '1' && obj.relation == 'wife') {
-                    $scope.rootWife = obj;
-                }
-            }
-        });
-
-
-    $scope.getTree = function( id ) {
-        for (var i=0; i<$scope.familyTree.length; i++) {
-            var obj = $scope.familyTree[i];
-            if( obj.relationWith == id && obj.relation == 'son') {
-                for (var j=0; j<$scope.familyTree.length; j++) {
-                    var object = $scope.familyTree[j];
-                    if( object.relationWith == obj.id && object.relation == 'wife') {
-                        obj.spouse = object;
-                    }
-                }
-                $scope.sons.push( obj );
-            }
-        }
-
-        for (var i=0; i<$scope.familyTree.length; i++) {
-            var obj = $scope.familyTree[i];
-            if( obj.relationWith == id && obj.relation == 'daughter') {
-                for (var j=0; j<$scope.familyTree.length; j++) {
-                    var object = $scope.familyTree[j];
-                    if( object.relationWith == obj.id && object.relation == 'husband') {
-                        obj.spouse = object;
-                    }
-                }
-                $scope.daughters.push( obj );
-            }
-        }
-    }
-});
-
 app.directive("tree", function($compile) {
     return {
         restrict: "E",
@@ -56,7 +7,7 @@ app.directive("tree", function($compile) {
         template:
         '<ul>'+
             '<li>' +
-                '<a href="#" ng-repeat="data in family" ng-controller="mainController" ng-click="getTree(data.id)">' +
+                '<a href="#" ng-repeat="data in family" ng-controller="mainController" ng-click="getTree(data.id)" ng-keydown="">' +
                     '<div class="panel panel-default">' +
                         '<div class="panel-heading">' +
                             '<h3 class="panel-title">'+
@@ -87,4 +38,5 @@ app.directive("tree", function($compile) {
         }
     };
 });
+
 
--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,3 +1,48 @@
-app.controller( 'mainController', function( $scope, $http ) {
+app.controller("mainController", function($http, $scope) {
+    $scope.familyTree = null;
+    $scope.sons = [];
+    $scope.daughters = [];
+    $scope.husbands = [];
+    $scope.wives = [];
+    $http.get( 'data/familyTree.json')
+        .success( function( data ) {
+            $scope.familyTree = data;
 
+            //assuming that root has an id:1 always
+            for (var i=0; i<$scope.familyTree.length; i++) {
+                var obj = $scope.familyTree[i];
+                if( obj.relationWith == '1' && obj.relation == 'wife') {
+                    $scope.rootWife = obj;
+                }
+            }
+        });
+
+
+    $scope.getTree = function( id ) {
+        for (var i=0; i<$scope.familyTree.length; i++) {
+            var obj = $scope.familyTree[i];
+            if( obj.relationWith == id && obj.relation == 'son') {
+                for (var j=0; j<$scope.familyTree.length; j++) {
+                    var object = $scope.familyTree[j];
+                    if( object.relationWith == obj.id && object.relation == 'wife') {
+                        obj.spouse = object;
+                    }
+                }
+                $scope.sons.push( obj );
+            }
+        }
+
+        for (var i=0; i<$scope.familyTree.length; i++) {
+            var obj = $scope.familyTree[i];
+            if( obj.relationWith == id && obj.relation == 'daughter') {
+                for (var j=0; j<$scope.familyTree.length; j++) {
+                    var object = $scope.familyTree[j];
+                    if( object.relationWith == obj.id && object.relation == 'husband') {
+                        obj.spouse = object;
+                    }
+                }
+                $scope.daughters.push( obj );
+            }
+        }
+    };
 });
